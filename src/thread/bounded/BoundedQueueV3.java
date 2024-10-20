2357446461ebaaf52621bf228a8561e7d2e4a96c
happyprogfrog <happyprogfrog@gmail.com>
2024-10-20 21:04:52 +0900

    [8. 생산자 소비자 문제 1] 예제 3

---
+++ src/thread/bounded/BoundedQueueV3.java
@@ -0,0 +1,56 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BoundedQueueV3 implements BoundedQueue {
+
+    private final Queue<String> queue = new ArrayDeque<>();
+    private final int max;
+
+    public BoundedQueueV3(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public synchronized void put(String data) {
+        while (queue.size() == max) {
+            log("[put] 큐가 가득 참, 생산자 대기");
+            try {
+                wait(); // RUNNABLE -> WAITING, 락 반납
+                log("[put] 생산자 깨어남");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        queue.offer(data);
+        log("[put] 생산자 데이터 저장, notify() 호출");
+        notify(); // 대기 스레드가 있다면, WAITING -> BLOCKED
+    }
+
+    @Override
+    public synchronized String take() {
+        while (queue.isEmpty()) {
+            log("[take] 큐에 데이터가 없음, 소비자 대기");
+            try {
+                wait();
+                log("[take] 소비자 깨어남");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        String data = queue.poll();
+        log("[take] 소비자 데이터 획득, notify() 호출");
+        notify(); // 대기 스레드가 있다면, WAITING -> BLOCKED
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        // 원칙적으로 synchronized를 추가해야하지만, 간단한 예제를 위해서 생략한다
+        return queue.toString();
+    }
+}
