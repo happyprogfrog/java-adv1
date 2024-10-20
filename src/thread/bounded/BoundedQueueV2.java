7b4b6f4d140243e367ce4e6ecb1b67144bffcf8f
happyprogfrog <happyprogfrog@gmail.com>
2024-10-20 17:53:21 +0900

    [8. 생산자 소비자 문제 1] 예제 2

---
+++ src/thread/bounded/BoundedQueueV2.java
@@ -0,0 +1,42 @@
+package thread.bounded;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BoundedQueueV2 implements BoundedQueue {
+
+    private final Queue<String> queue = new ArrayDeque<>();
+    private final int max;
+
+    public BoundedQueueV2(int max) {
+        this.max = max;
+    }
+
+    @Override
+    public synchronized void put(String data) {
+        while (queue.size() == max) {
+            log("[put] 큐가 가득 참, 생산자 대기");
+            sleep(1000);
+        }
+        queue.offer(data);
+    }
+
+    @Override
+    public synchronized String take() {
+        while (queue.isEmpty()) {
+            log("[take] 큐에 데이터가 없음, 소비자 대기");
+            sleep(1000);
+        }
+
+        return queue.poll();
+    }
+
+    @Override
+    public String toString() {
+        // 원칙적으로 synchronized를 추가해야하지만, 간단한 예제를 위해서 생략한다
+        return queue.toString();
+    }
+}
